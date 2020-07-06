502541de2c5a8c559144343862946347dad0b733
Grzegorz <grzegorzchoinski97@gmail.com>
2020-07-06 15:45:32 +0200

    Project base (#1)

---
+++ Api/Api/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly string msg = "Hello there!";
+
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return msg;
+        }
+    }
+}
