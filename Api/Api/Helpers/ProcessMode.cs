66c09304587485669d4df075b04309c860f156d6
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-08-06 17:42:49 +0200

    Auto mode for script processing

--- Api/Api/Helpers/ProcessMode.cs
+++ Api/Api/Helpers/ProcessMode.cs
@@ -6,6 +6,7 @@ namespace Api.Models
     /// <summary>
     /// Mode, in which an image file of the graph entity is processed by the script.
     /// <br></br>
+    /// <para>AUTO- Sript tries to determine background by itself.</para>
     /// <para>GRID_BG - Hand drawn on grid/lined piece of paper (grid/lined notebook etc.)</para>
     /// <para>CLEAN_BG - Hand drawn on empty uniform color background (on board, empty piece of paper, editor (paint)</para>
     /// <para>PRINTED - Printed (e.g. from paper, publication, book...)</para>
@@ -13,6 +14,7 @@ namespace Api.Models
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ProcessMode
     {
+        AUTO = 0,
         GRID_BG = 1,
         CLEAN_BG = 2,
         PRINTED = 3
